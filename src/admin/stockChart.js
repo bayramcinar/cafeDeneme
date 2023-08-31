e350787a8fb3746975c11e192ba46cdbfbe34b73
Bayram Çınar <99193151+bayramcinar@users.noreply.github.com>
2023-08-31 12:09:21 +0300

    stockChart.js

--- src/admin/stockChart.js
+++ src/admin/stockChart.js
@@ -9,7 +9,7 @@ function StockChart({categoryID}) {
     const [title,setTitle] = useState("");
          
     useEffect(() => {
-        axios.get(`http://localhost:8081/getAllStock/${categoryID}`)
+        axios.get(`https://cafeapp-y5se.onrender.com/getAllStock/${categoryID}`)
         .then(res => {
             const isimler = res.data.map(item => item.isim); 
             setLabels(isimler); 
@@ -19,7 +19,7 @@ function StockChart({categoryID}) {
     }, []);
     
     useEffect(() => {
-        axios.get(`http://localhost:8081/getCategoryName/${categoryID}`)
+        axios.get(`https://cafeapp-y5se.onrender.com/getCategoryName/${categoryID}`)
         .then(res => {
             setTitle(res.data[0].kategoriName);
         })
