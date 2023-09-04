3095b03efc219f3cce0c98ada9f8c878471a5a63
Bayram Çınar <99193151+bayramcinar@users.noreply.github.com>
2023-09-04 13:29:29 +0300

    tables.js

--- src/admin/tables.js
+++ src/admin/tables.js
@@ -1,31 +1,94 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import table from "../img/chair.png";
+import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
+import NotificationsOffIcon from '@mui/icons-material/NotificationsOff';
 import "../style/tables.css";
+import { ToastContainer, toast } from 'react-toastify';
 
 function Tables() {
   const [tables, setTables] = useState([]);
 
+  const successToast = () => {
+    toast.success('Masa Aktif Edildi !', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light"
+    });
+} 
+
+const errorToast = () => {
+  toast.error('Masa Deaktif Edildi !', {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light"
+  });
+} 
+
   useEffect(() => {
     axios.get("https://cafeapp-y5se.onrender.com/getAllTables")
       .then(res => {
+        const tableDataWithStatus = res.data.map(tableData => ({
+          ...tableData,
+          isActive: tableData.isActive === 1 
+        }));
         setTables(res.data);
       })
       .catch(err => console.log(err));
-  }, []);
+  });
+
+  const setActive = (masaID) =>{
+    axios.post(`https://cafeapp-y5se.onrender.com/setActive/${masaID}`)
+      .then(res => {
+        successToast();
+      })
+      .catch(err => console.log(err))
+  }
+
+  const setDeactive = (masaID) =>{
+    axios.post(`https://cafeapp-y5se.onrender.com/setDeactive/${masaID}`)
+      .then(res => {
+        errorToast();
+      })
+      .catch(err => console.log(err))
+  }
 
   const renderTable = (tableData) => (
-    <div key={tableData.id} className='bos col-lg-2 col-md-3 col-sm-6 mb-4'> {/* Değişiklik burada */}
+    <div key={tableData.id} className='bos col-lg-2 col-md-3 col-sm-6 mb-4'> 
       <div className='tableBox'>
         <div className='row genelBox'>
-          <div className='col-6'>
+          <div className='col-4'>
             <img className='tableIcon' src={table} alt={`Table ${tableData.tableNumber}`} />
           </div>
-          <div className='col-3'>
+          <div className='col-2'>
             <h5 className='tableNumber'>{tableData.id}</h5>
+          </div>
+          <div className='col-3' >
+            {tableData.isActive ? (
+              <div onClick={() => setDeactive(tableData.id)} style={{display:"block",alignItems:"center",justifyContent:"center"}}>
+              <NotificationsOffIcon sx={{color:"green",width:40,cursor:"pointer",marginTop:"5px"}}/>
+              <span className="statusText" style={{color:"white"}}>Active</span>
+              </div>
+            ) : (
+              <div onClick={() => setActive(tableData.id)} style={{display:"block",alignItems:"center",justifyContent:"center"}}>
+              <NotificationsActiveIcon sx={{color:"red",width:40,cursor:"pointer",marginTop:"5px"}}/>
+              <span className="statusText" style={{color:"white"}}>Inactive</span>
+              </div>
+            )}
           </div>
         </div>
       </div>
+            <ToastContainer />      
     </div>
   );
 
@@ -45,7 +108,7 @@ function Tables() {
 
   return (
     <div>
-      <h1 className='titleTable'>Tables</h1>  
+      <h1 className='titleTable' style={{textDecoration:"underline"}}>Tables</h1>  
       {renderTableRows()}
     </div>
   );
